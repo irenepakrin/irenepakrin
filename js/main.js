e4b7c3b60bd0ac1294d790895a4a407763898d8a
irenepakrin <100677632+irenepakrin@users.noreply.github.com>
2022-04-30 14:49:58 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -167,24 +167,24 @@ window.addEventListener('DOMContentLoaded', function () {
 
     /*  slide,click event section */
 
-    const wrapper = document.querySelector('#section02 .container');
-    let count = -500;
+    // const wrapper = document.querySelector('#section02 .container');
+    // let count = -500;
 
-    const addSnow = () => {
-    const snow = document.createElement('span');
-    snow.classList.add('snow');
-    snow.style.left = `${Math.random() * (window.innerWidth - 1) + 1}px`;
-    snow.style.animationDuration = `${Math.random() * (20 - 8) + 8}s`;
-    snow.style.animationDelay = `${Math.random() * (10 - 1) + 1}s`;
-    snow.style.opacity = `${Math.random()}`;
-    wrapper.append(snow);
-    if (count < 300) {
-        window.requestAnimationFrame(addSnow);
-        count++;
-    }
-    };
+    // const addSnow = () => {
+    // const snow = document.createElement('span');
+    // snow.classList.add('snow');
+    // snow.style.left = `${Math.random() * (window.innerWidth - 1) + 1}px`;
+    // snow.style.animationDuration = `${Math.random() * (20 - 8) + 8}s`;
+    // snow.style.animationDelay = `${Math.random() * (10 - 1) + 1}s`;
+    // snow.style.opacity = `${Math.random()}`;
+    // wrapper.append(snow);
+    // if (count < 300) {
+    //     window.requestAnimationFrame(addSnow);
+    //     count++;
+    // }
+    // };
 
-    addSnow();
+    // addSnow();
 
 })
 
